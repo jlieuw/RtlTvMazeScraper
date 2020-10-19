dabaeca0d5e0d0a6ff4e0e204fbb0d73312792c3
Joeri Lieuw <joeri.lieuw@robidus.nl>
2020-10-19 13:50:20 +0200

    polly retry-after

--- TvMazeScraper.Web/PolicyHandler.cs
+++ TvMazeScraper.Web/PolicyHandler.cs
@@ -1,27 +1,37 @@
 ﻿using Polly;
 using Polly.Extensions.Http;
-using Polly.Timeout;
 using System;
 using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
 
 namespace TvMazeScraper
 {
     public static class PolicyHandler
     {
-        public static IAsyncPolicy<HttpResponseMessage> WaitAndRetry(int retryCount = 5) =>
+        public static IAsyncPolicy<HttpResponseMessage> WaitAndRetry() =>
             HttpPolicyExtensions
                 .HandleTransientHttpError()
-                .Or<HttpRequestException>()
-                .Or<TimeoutRejectedException>()
                 .OrResult(msg => msg.StatusCode == System.Net.HttpStatusCode.TooManyRequests)
-                .WaitAndRetryAsync(retryCount, retryAttempt => GetRetryTime(retryAttempt));
+                .WaitAndRetryAsync(retryCount: 5,
+            sleepDurationProvider: (retryCount, response, context) =>
+            {
+                var serverWaitDuration = GetServerWaitDuration(response.Result?.Headers.RetryAfter);
+                var waitDuration = serverWaitDuration.TotalMilliseconds;
+                return TimeSpan.FromMilliseconds(waitDuration);
+            },
+                    onRetryAsync: (e, ts, i, ctx) => Task.CompletedTask);
 
+        private static TimeSpan GetServerWaitDuration(RetryConditionHeaderValue retryAfter)
+        {
+            if (retryAfter == null)
+                return TimeSpan.Zero;
+
+            return retryAfter.Date.HasValue
+                ? retryAfter.Date.Value - DateTime.UtcNow
+                : retryAfter.Delta.GetValueOrDefault(TimeSpan.Zero);
+        }
         public static IAsyncPolicy<HttpResponseMessage> Timeout(int seconds = 2) =>
             Policy.TimeoutAsync<HttpResponseMessage>(TimeSpan.FromSeconds(seconds));
-
-        private static TimeSpan GetRetryTime(int retryAttempt)
-        {
-            return TimeSpan.FromSeconds(Math.Pow(2, retryAttempt));
-        }
     }
 }
